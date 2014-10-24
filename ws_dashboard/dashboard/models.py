ec9aae04596823f13efb4b533000072c2e2fd6e4
antonio@mock-b2b <antonio.davoli@gmail.com>
2014-10-24 20:06:38 +0000

    First Model Definition

--- ws_dashboard/dashboard/models.py
+++ ws_dashboard/dashboard/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
+PHONE_INTERACTION_CHOICES = (
+    (1, 'INCOMING'),
+    (2, 'OUTGOING')
+)
+
+COMPANY_ROLES = (
+    (1, 'BOSS'),
+    (2, 'SUPERBOSS')
+)
+
+class Company(models.Model): 
+    name = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)    
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    role = models.IntegerField(choices=COMPANY_ROLES)
+    phone_number = models.CharField(max_length=30)
+    company = models.ForeignKey(Company, related_name="employees_company")
+
+class Call(models.Model):
+    owner = models.ForeignKey(Employee, related_name="calls_employee")
+    caller = models.CharField(max_length=30)
+    receiver = models.CharField(max_length=30)
+    start_time = models.DateTimeField()
+    length = models.IntegerField()
+    # GPS/Android Information
+    call_type = models.IntegerField(choices=PHONE_INTERACTION_CHOICES)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.receiver 
+
+class Message(models.Model): 
+    owner = models.ForeignKey(Employee, related_name="messages_employee")
+    sender = models.CharField(max_length=30)
+    receiver = models.CharField(max_length=30)
+    sent_time = models.DateTimeField()
+    message_type = models.IntegerField(choices=PHONE_INTERACTION_CHOICES)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.receiver
+
+class Data(models.Model): 
+    owner = models.ForeignKey(Employee, related_name="data_employee")
+    volume = models.FloatField(help_text="Amount of kb exchanged")
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+    start_time = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.volume
+
+
 # Create your models here.
